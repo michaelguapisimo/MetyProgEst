09540552b8fee9f0ab569ac2c038e8bf4125e8b9
michaelguapisimo <mramirezc@uamv.edu.ni>
2024-08-21 12:08:42 -0600

    Saludar

---
+++ Saludar/Saludar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Imprime "Hola Mundo" en la consola
+            Console.WriteLine("Hola Mundo");
+
+            // Pausa la consola para que el usuario pueda ver el mensaje antes de que la aplicación se cierre
+            Console.WriteLine("Presiona cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Saludar/Saludar/Saludar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Saludo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicita el nombre del usuario
+            Console.Write("Por favor, ingrese su nombre: ");
+            string nombre = Console.ReadLine();
+
+            // Saluda al usuario
+            Console.WriteLine("Hola, " + nombre + "! Bienvenido.");
+
+
+        }
+    }
+}
